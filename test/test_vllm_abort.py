a5766b0785f748d1e6bd2021cf9042fe418752a7
Myrick <myrick@MyrickdeMacBook-Pro.local>
2025-06-19 09:42:19 +0800

    abort test dtype=float16

--- test/test_vllm_abort.py
+++ test/test_vllm_abort.py
@@ -34,7 +34,8 @@ async def test_vllm_abort():
             gpu_memory_utilization=0.3,  # 使用较少内存
             tensor_parallel_size=1,
             trust_remote_code=True,
-            enable_chunked_prefill=False
+            enable_chunked_prefill=False,
+            dtype='float16',
         )
         
         # 2. 创建引擎
